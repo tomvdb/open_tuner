21b506582998f46c00bf3ead34ccc850f0be1bd4
Martin, DL1RF <dl1rf@users.noreply.github.com>
2024-05-13 07:26:02 +0200

    Update VideoViewForm.Designer.cs
    Added Background Color Black

--- MediaPlayers/VideoViewForm.Designer.cs
+++ MediaPlayers/VideoViewForm.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
             this.Name = "VideoViewForm";
